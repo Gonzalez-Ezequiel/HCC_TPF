21431b5e715ecaddee1ddcbfd0aec16cdd4da6f4
Ezequiel <ezequielgonz256@gmail.com>
2024-08-01 05:57:43 -0300

    minor bug fixing

--- primero.py
+++ primero.py
@@ -1,16 +1,14 @@
 from manim import *
-from SYD.subrutinas import *
-from SYD.entradas import *
-
+from Inputs.datos1 import *
+from Func.funcionestex import *
 class video(Scene):
 
     def construct(self):
 
-
         # planteo el reto
 
-        esctex2(self,reto) #escribo reto
-        bortex(self,reto) #borro reto
+        esctex2(self,reto)  #escribo reto
+        bortex(self,reto)  #borro reto
         self.wait(0.2)
 
         #presento el problema
@@ -20,8 +18,6 @@ class video(Scene):
         esctex2(self, condicion2)
         esctex2(self, condicion3)
         esctex2(self, objetivo)
-        self.wait(4)
-
+        self.wait(4.0)
         #borro
-
         self.play(FadeOut(condicion1[0], condicion2[0], condicion3[0], enunciado[0], objetivo[0]))
